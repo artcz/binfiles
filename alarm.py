a96217acc712e36586b83396640667038b7bed8e
Artur Czepiel <czepiel.artur@gmail.com>
2019-11-19 08:52:45 +0100

    added alarm.py

---
+++ alarm.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3.6
+# Using 3.6 because system python and `apt install python3-gi`
+
+import sys
+import time
+from datetime import datetime
+
+import gi
+
+gi.require_version('Notify', '0.7')
+from gi.repository import Notify  # NOQA
+
+at, message = sys.argv[1], ' '.join(sys.argv[2:])
+
+hh, mm = at[:2], at[2:]
+
+alert_at = datetime.now().replace(hour=int(hh), minute=int(mm), second=0)
+
+
+print(alert_at, message)
+
+Notify.init(f"Scheduled {message} for {alert_at}")
+
+notification = Notify.Notification.new(
+    f"Alert {' '*100}",
+    message,
+    "dialog-information",
+)
+notification.set_urgency(2)
+notification.set_timeout(0)
+
+time.sleep(int(abs((datetime.now() - alert_at).total_seconds())))
+notification.show()
